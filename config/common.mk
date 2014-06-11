0b34816bf5f33acb2d8684a3fc548cb457837f93
David Vaz <vazdavid@icloud.com>
2014-06-11 13:28:30 +0200

    add gapps
    Change-Id: I310a786ef1a58e20741c85ee3e48561dc46289a5

--- config/common.mk
+++ config/common.mk
@@ -137,6 +137,38 @@ PRODUCT_PACKAGES += \
     libFFmpegExtractor \
     libnamparser
 
+##### GApps #####
+
+PRODUCT_COPY_FILES += \
+    vendor/google/system/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+    vendor/google/system/etc/permissions/com.google.android.camera2.xml:system/etc/permissions/com.google.android.camera2.xml \
+    vendor/google/system/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+    vendor/google/system/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
+    vendor/google/system/etc/permissions/features.xml:system/etc/permissions/features.xml \
+    vendor/google/system/etc/permissions/preferred-apps/google.xml:system/etc/permissions/preferred-apps/google.xml \
+    vendor/google/system/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar \
+    vendor/google/system/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
+    vendor/google/system/framework/com.google.android.camera2.jar:system/framework/com.google.android.camera2.jar \
+    vendor/google/system/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar \
+    vendor/google/system/lib/libAppDataSearch.so:system/lib/libAppDataSearch.so \
+    vendor/google/system/lib/libgames_rtmp_jni.so:system/lib/libgames_rtmp_jni.so \
+    vendor/google/system/lib/libgcastv2_base.so:system/lib/libgcastv2_base.so \
+    vendor/google/system/lib/libgcastv2_support.so:system/lib/libgcastv2_support.so \
+    vendor/google/system/lib/libjgcastservice.so:system/lib/libjgcastservice.so \
+    vendor/google/system/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+
+PRODUCT_COPY_FILES += \
+    vendor/google/system/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+    vendor/google/system/priv-app/GoogleBackupTransport.apk:system/priv-app/GoogleBackupTransport.apk \
+    vendor/google/system/priv-app/GoogleFeedback.apk:system/priv-app/GoogleFeedback.apk \
+    vendor/google/system/priv-app/GoogleLoginService.apk:system/priv-app/GoogleLoginService.apk \
+    vendor/google/system/priv-app/GoogleOneTimeInitializer.apk:system/priv-app/GoogleOneTimeInitializer.apk \
+    vendor/google/system/priv-app/GooglePartnerSetup.apk:system/priv-app/GooglePartnerSetup.apk \
+    vendor/google/system/priv-app/GoogleServicesFramework.apk:system/priv-app/GoogleServicesFramework.apk \
+    vendor/google/system/priv-app/Phonesky.apk:system/priv-app/Phonesky.apk \
+    vendor/google/system/priv-app/PrebuiltGmsCore.apk:system/priv-app/PrebuiltGmsCore.apk \
+    vendor/google/system/priv-app/SetupWizard.apk:system/priv-app/SetupWizard.apk
+
 # languages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
